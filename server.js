be0b274861cc70c76f1bf7d6f18351eaceb28c2e
Darth Clark <subflw47@gmail.com>
2026-04-06 19:31:54 -0500

    updated server.js from localhost serving

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 // Rate limiter: 5 recommendation requests per IP per hour
 const recommendationsLimiter = rateLimit({
